2a3fa4e8ff591979799c04ccf09c6057fdb55e3e
Artur_Timoshenko <42868871+artur-timoshenko@users.noreply.github.com>
2023-06-28 09:38:13 +0300

    Update Project sem4.cpp

--- Project sem4.cpp
+++ Project sem4.cpp
@@ -1,6 +1,3 @@
-﻿// Project sem4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
 #include <iostream>
 #include <vector>
 #include <map>
@@ -170,53 +167,45 @@ public:
 };
 
 int main() {
-    // Створення категорій
+
     Category* category1 = new Category("Electronics");
     Category* category2 = new Category("Clothing");
 
-    // Створення товарів
     Product* product1 = new Product("Smartphone", "A high-end smartphone with advanced features", 1000.0, category1);
     Product* product2 = new Product("Laptop", "A powerful laptop for professional use", 1500.0, category1);
     Product* product3 = new Product("T-Shirt", "A comfortable cotton t-shirt", 20.0, category2);
     Product* product4 = new Product("Jeans", "Stylish jeans made from premium denim", 50.0, category2);
 
-    // Створення каталогу
     Catalog catalog;
     catalog.addCategory(category1);
     catalog.addCategory(category2);
 
-    // Додавання товарів до категорій
     catalog.addProductToCategory(product1, category1);
     catalog.addProductToCategory(product2, category1);
     catalog.addProductToCategory(product3, category2);
     catalog.addProductToCategory(product4, category2);
 
-    // Створення користувачів та кошиків
     std::map<std::string, Cart*> users;
-    users["Alice"] = new CartDecorator();
-    users["Bob"] = new Cart();
+    users["Tom"] = new CartDecorator();
+    users["Jerry"] = new Cart();
 
-    // Додавання товарів до кошиків
-    users["Alice"]->addProduct(product1);
-    users["Alice"]->addProduct(product3);
-    users["Alice"]->addProduct(product2);
-    users["Alice"]->addProduct(product4);
-    users["Bob"]->addProduct(product2);
-    users["Bob"]->addProduct(product3);
-    users["Bob"]->addProduct(product4);
+    users["Tom"]->addProduct(product1);
+    users["Tom"]->addProduct(product3);
+    users["Tom"]->addProduct(product2);
+    users["Tom"]->addProduct(product4);
+    users["Jerry"]->addProduct(product2);
+    users["Jerry"]->addProduct(product3);
+    users["Jerry"]->addProduct(product4);
 
-    // Виведення замовлень та загальної ціни для кожного користувача
     for (const auto& user : users) {
         std::cout << "User: " << user.first << std::endl;
         user.second->printCart();
         std::cout << std::endl;
     }
 
-    // Видалення товарів з кошиків
-    users["Alice"]->removeProduct(product1);
-    users["Bob"]->removeProduct(product2);
+    users["Tom"]->removeProduct(product1);
+    users["Jerry"]->removeProduct(product2);
 
-    // Виведення оновлених замовлень та загальної ціни для кожного користувача
     std::cout << "Updated Carts:" << std::endl;
     for (const auto& user : users) {
         std::cout << "User: " << user.first << std::endl;
@@ -224,20 +213,10 @@ int main() {
         std::cout << std::endl;
     }
 
-    // Звільнення пам'яті
     for (const auto& user : users) {
         delete user.second;
     }
 
     return 0;
 }
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
